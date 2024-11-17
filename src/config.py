f378343e7c360ea4c0ef313ff7032178412b2099
Sirkkeli <siiri.kalliomaki@helsinki.fi>
2024-11-17 15:52:14 +0200

    layout.html muokattu niin että sovellus näyttää kivemmalta. .gitignore lisätty venv, koska jostain syystä se oli muodossa .venv

--- src/config.py
+++ src/config.py
@@ -8,8 +8,8 @@ load_dotenv()
 
 test_env = getenv("TEST_ENV") == "true"
 print(f"Test environment: {test_env}")
-
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+#en saanut toimimaan. Koita korvata getenv("DATABASE_URL") serverin linkillä
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
